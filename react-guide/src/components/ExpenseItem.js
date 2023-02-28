082e8a12044971c57c145911f6661518c3243ca3
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-02-28 15:37:18 +0530

    ptc: Adding styles

--- react-guide/src/components/ExpenseItem.js
+++ react-guide/src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
   return (
     <div>
-      <div>28 march</div>
-      <div>Bike Insurance</div>
-      <div>2456</div>
+      <div className="expense-item">28 march</div>
+      <div className="expense-item__description">
+        <h2>Bike Insurance </h2>
+      </div>
+      <div className="expense-item__price">2456</div>
     </div>
   );
 }
