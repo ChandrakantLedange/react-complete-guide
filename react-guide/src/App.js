9cc85b13c9cae4fabc823484a8a975d9e07c5ac6
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-02-28 15:19:58 +0530

    ptc: Custom component

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <div className="App">let's get started</div>;
+  return (
+    <div>
+      let's get started
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
