b4be34756c9d22f8d46c6f07b740f351926e4fa2
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-03-01 15:31:23 +0530

    ptc: composition (Children props)

--- react-guide/src/components/ExpenseItem.js
+++ react-guide/src/components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import React from "react";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
   return (
-    <div>
-      <div className="expense-item">
-        <ExpenseDate date={props.date} />
-      </div>
-      <div className="expense-item__description">
-        <h2>{props.title} </h2>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">{props.title}</div>
       <div className="expense-item__price">{props.amount}</div>
-    </div>
+    </Card>
   );
 }
