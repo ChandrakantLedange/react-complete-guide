f435b25b9f1d5154c2f1fd973dcf60ffef2a99a8
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-03-02 22:27:35 +0530

    ptc: Adding form input

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   return (
     <div>
+      <NewExpense />
       <Expenses />
     </div>
   );
