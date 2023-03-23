770345a63146aa233fbee02a303e58de5489a711
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-03-23 20:11:08 +0530

    ptc: showing add new expense conditionally

--- react-guide/src/components/NewExpense/NewExpense.js
+++ react-guide/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+  const handleAddNewExpense = () => {
+    setIsEditing(true);
+  };
+  const handleCancel = () => {
+    setIsEditing(false);
   };
 
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={handleAddNewExpense}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={handleCancel}
+        />
+      )}
     </div>
   );
 };
