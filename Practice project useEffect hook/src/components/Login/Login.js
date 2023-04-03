5298a7d74eb3eb7fef84c675df03dfac561b89a1
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-04-03 21:37:15 +0530

    ptc:  useEffect with cleanup

--- Practice project useEffect hook/src/components/Login/Login.js
+++ Practice project useEffect hook/src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const timer = setTimeout(() => {
+      console.log("form validity");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      console.log("clean up ");
+      clearTimeout(timer);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
