3e6ed21822a7887ba7a9dc5c4b675775c927c160
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-06-23 23:51:40 +0530

    ptc: modal open and close / passing event via props

--- practice project The Food order App/src/components/Cart/Cart.js
+++ practice project The Food order App/src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul>
       {[
@@ -16,14 +16,16 @@ const Cart = () => {
     </ul>
   );
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Ammput</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
