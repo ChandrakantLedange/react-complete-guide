c57d01b7002d6267f97265218dd360ddbd3710ae
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-07-06 01:00:30 +0530

    ptc: provide store to the application

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react";
+import "./index.css";
+import App from "./App";
+import Store from "./store";
 
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>
+);
