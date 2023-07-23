ef95c77781ff585e5378f4b5a76e3e3201837aca
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-07-23 23:22:13 +0530

    ptc: navigating programmatically

--- React Router/src/pages/Home.js
+++ React Router/src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+  function handleNavigation() {
+    navigate("/products");
+  }
   return (
     <div>
       <h1>Home Page</h1>
       <p>
         <Link to="/products">Go to products</Link>
       </p>
+      <button onClick={handleNavigation}>Products navigation</button>
     </div>
   );
 }
