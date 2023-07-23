c77af80203cc001744eb2a85e9504b6afb4bb954
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-07-23 22:24:12 +0530

    ptc: Outlet and navigation header

--- React Router/src/App.js
+++ React Router/src/App.js
@@ -2,10 +2,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import ErrorHandler from "./pages/ErrorHandler";
 import Products from "./pages/Products";
+import Root from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home />, errorElement: <ErrorHandler /> },
-  { path: "/products", element: <Products />, errorElement: <ErrorHandler /> },
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <Home />, errorElement: <ErrorHandler /> },
+      {
+        path: "/products",
+        element: <Products />,
+        errorElement: <ErrorHandler />,
+      },
+    ],
+  },
 ]);
 
 function App() {
