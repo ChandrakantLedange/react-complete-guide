5ca38370e7e086a2d802ef7cb97bb2eaabf9fd7f
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-07-31 00:55:15 +0530

    PTC: mock async data as function

--- Unit Testing React App/src/components/Async.test.js
+++ Unit Testing React App/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe("Async ", () => {
   test("should render list items", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "first post" }],
+    });
     render(<Async />);
 
     const listitemElements = await screen.findAllByRole("listitem");
